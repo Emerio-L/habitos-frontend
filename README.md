7a03bfcf459b68d2c367eaffee4eecd5c6ab71f1
Emerio-L <eluceromilian@gmail.com>
2026-03-02 23:14:15 -0600

    Semana 2: frontend listo para integración GET

---
+++ README.md
@@ -0,0 +1,37 @@
+# Habitos Frontend
+
+Frontend de la aplicación de gestión de hábitos basada en el libro *Hábitos Atómicos* de James Clear.  
+Desarrollada con **Next.js**, **React** y **Redux Toolkit**, y con  datos desde un backend **Express.js** conectado a **MongoDB**.
+
+---
+
+## Tecnologías
+
+- Next.js, React
+- Redux Toolkit
+- Fetch API
+- Node.js
+
+
+## Instalación de dependencias
+
+En la terminal ejecutar el comando
+npm install
+
+## Ejecutar la aplicacion
+
+en la terminal con el comando 
+npm run dev
+
+La app correrá por defecto en http://localhost:3001
+
+Asegúrate de que el backend esté corriendo en http://localhost:3000
+
+## Instalación y ejecución
+
+
+1. Clonar el repositorio:
+
+
+git clone <URL_DEL_REPOSITORIO> habitos-frontend
+cd habitos-frontend
---
+++ app/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { fetchHabitos } from "@/store/slices/habitosSlice";
+
+export default function Home() {
+  const dispatch = useAppDispatch();
+  const { habitos, loading } = useAppSelector((state) => state.habitos);
+
+  useEffect(() => {
+    dispatch(fetchHabitos());
+  }, [dispatch]);
+  
+
+  console.log(habitos);
+console.log(loading);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-900 text-white">
+      <div>
+        <h1 className="text-3xl font-bold mb-4">Habitos</h1>
+
+        {loading && <p>Cargando...</p>}
+
+        <ul>
+          {habitos.map((habito) => (
+            <li key={habito._id}>{habito.nombre}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
